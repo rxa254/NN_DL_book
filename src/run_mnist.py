46e160af3dfaa099d2306285f5dd061581786bb5
Rana X Adhikari <rana@caltech.edu>
2024-03-30 09:28:36 -0700

    updates to handle the YAML file for params

--- src/run_mnist.py
+++ src/run_mnist.py
@@ -1,15 +1,27 @@
+import yaml
 import mnist_loader
 import network
+from timeit import default_timer as timer
+
+# Load configuration from the YAML file
+with open('network_config.yaml', 'r') as file:
+    config = yaml.safe_load(file)
 
 
-print("Load the MNIST database...")
 training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
+print("Loaded the MNIST database...")
 
 
-print("Initializing the NN...")
-net = network.Network([784, 30, 10])
+
+# Initialize the Network with configurations from YAML file
+net = network.Network(config['network_structure'], config['activation_functions'])
+print("Thinking Machine Awakened...")
+
 
 print("Training the NN...")
-net.SGD(training_data, 30, 10, 3.0, test_data=test_data)
+t0 = timer()
+# Using parameters from the configuration file
+net.SGD(training_data, config['epochs'], config['mini_batch_size'], config['learning_rate'], test_data=test_data)
 
-print("Done Training and Testing.")
+print("Done Training and Testing Ex Machina.")
+print("Trainging time = {:0.1f}".format(timeit() - t0) + " seconds.")
